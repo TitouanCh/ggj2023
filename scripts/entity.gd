968c80b2b8126f537007a3aee33d15608c6c4590
TitouanCh <titouan.azert@wadsa.com>
2023-02-03 22:06:56 +0100

    player

--- scripts/entity.gd
+++ scripts/entity.gd
@@ -1,3 +1,5 @@
 extends KinematicBody2D
 
 class_name Entity
+
+var accel = Vector2(50, 50)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,20 @@
 extends Entity
 
 func _process(delta):
-	pass
+	# - MOVEMENT
+	var inputs = Vector2.ZERO
+	if Input.is_action_pressed("left"):
+		inputs.x -= 1
+	if Input.is_action_pressed("right"):
+		inputs.x += 1
+	if Input.is_action_pressed("up"):
+		inputs.y -= 1
+	if Input.is_action_pressed("down"):
+		inputs.y += 1
+	
+	position += inputs * delta * accel
+
+
+func _draw():
+	# - DEBUG
+	draw_circle(Vector2.ZERO, 4, Color(255, 255, 255))
